94627d8ec32b516fcee61048f6edf1a0481b2a26
ujjalcal <ujjal.b@gmail.com>
2023-03-06 04:54:14 +0000

    otel

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
-const AWS = require('aws-sdk');
-const dynamodb = new AWS.DynamoDB.DocumentClient();
+const { registerInstrumentations } = require('@opentelemetry/instrumentation');
+const { AwsLambdaInstrumentation } = require('@opentelemetry/instrumentation-aws-lambda');
+const { HttpInstrumentation } = require('@opentelemetry/instrumentation-http');
 
+registerInstrumentations({
+  instrumentations: [
+    new AwsLambdaInstrumentation(),
+    new HttpInstrumentation(),
+  ],
+});
+
+
+const AWS = require('aws-sdk');
+const { AwsSdkInstrumentation } = require('@opentelemetry/instrumentation-aws-sdk');
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+new AwsSdkInstrumentation().enableModule(AWS, [ 'DynamoDB' ]);
+
+
+const { context } = require('@opentelemetry/api');
 exports.handler = async (event) => {
- 
+  const span = context.active().startSpan('put-dynamodb-item');
+  span.setAttribute('aws.region', process.env.AWS_REGION);
+  span.setAttribute('aws.accountId', context.invokedFunctionArn.split(':')[4]);
+  span.setAttribute('aws.lambda.arn', context.invokedFunctionArn);
+  span.setAttribute('aws.lambda.name', context.functionName);
 
   const params = {
     TableName: 'test-table',
@@ -18,9 +39,13 @@ exports.handler = async (event) => {
   try {
     const data = await dynamodb.put(params).promise();
     console.log('Successfully wrote data to DynamoDB table.');
+    span.setStatus({ code: 200 });
+    span.end();
     return { statusCode: 200, body: JSON.stringify(data) };
   } catch (err) {
     console.log(err);
+    span.setStatus({ code: 500, message: err.message });
+    span.end();
     return { statusCode: 500, body: err };
   }
 };
