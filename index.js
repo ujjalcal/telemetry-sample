683ce0bcc5e7d1ad62e2b0e0fe3114bb6f99204d
ujjalcal <ujjal.b@gmail.com>
2023-03-06 04:51:20 +0000

    hello-world working

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
-//const AWS = require('aws-sdk');
-//const dynamodb = AWSXRay.captureAWSClient(new AWS.DynamoDB.DocumentClient());
-const AWSXRay = require('aws-xray-sdk-core');
-const AWS = AWSXRay.captureAWS(require('aws-sdk'));
+const AWS = require('aws-sdk');
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
-
 exports.handler = async (event) => {
-  AWSXRay.captureAWSClient(dynamodb);
-  //const { id, name, key1, key2, key3 } = event;
+ 
 
   const params = {
     TableName: 'test-table',
@@ -21,7 +16,6 @@ exports.handler = async (event) => {
   };
 
   try {
-    
     const data = await dynamodb.put(params).promise();
     console.log('Successfully wrote data to DynamoDB table.');
     return { statusCode: 200, body: JSON.stringify(data) };
